db70d5addd2bb604acb3d2df7ecf94c0abb3d316
Юлия Ушенина <ushenina.julia@gmail.com>
2022-07-28 12:14:12 +0300

    Second_bugs

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,8 +7,14 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [
+    '158.160.6.112',
+    'localhost',
+    'ushenina.hopto.org',
+    '127.0.0.1',
+    '[::1]',
+    'testserver',
+]
 
 # Application definition
 
